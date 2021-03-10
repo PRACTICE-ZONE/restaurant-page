53de3651cf7ba2c5b67cb5d3bed98cdbb79bc07e
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-10 19:48:34 +0000

    rails db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_190102) do
+ActiveRecord::Schema.define(version: 2021_03_10_194744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "names", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sections", force: :cascade do |t|
     t.string "name"
