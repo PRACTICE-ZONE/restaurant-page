bc65e433cabcb4e1d2bb2554a0625325563682dd
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-11 20:39:40 +0000

    seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "seeding data"
+
+if photo.count > 0 
+    puts "WARNING: There is existing data"
+    puts "To reseed, first go to rails console and type Photo.delete.all"
+    puts "For heroku type heroku run rails console"
+    exit
+end
+
+Photo.create!(url: )
