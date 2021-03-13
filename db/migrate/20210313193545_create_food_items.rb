0997463a808a0f0b5df2edf72edbdd27acda13b8
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-13 19:35:59 +0000

    adding another migration

--- db/migrate/20210313193545_create_food_items.rb
+++ db/migrate/20210313193545_create_food_items.rb
@@ -2,6 +2,7 @@ class CreateFoodItems < ActiveRecord::Migration[5.2]
   def change
     create_table :food_items do |t|
       t.string :name
+      t.decimal :price
       t.references :section, foreign_key: true
 
       t.timestamps
