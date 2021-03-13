75c7cfd606f2a45c270a912178adc51ed31b8899
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-13 20:21:49 +0000

    removing the schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "names", force: :cascade do |t|
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "names", force: :cascade do |t|
+  #   t.string "image"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
 end
