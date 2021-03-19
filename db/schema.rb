9f624e2fe6712a25e05d1ff2f77930590d32ca15
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-19 20:26:27 +0000

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_195446) do
+ActiveRecord::Schema.define(version: 2021_03_19_202531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_195446) do
     t.bigint "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "Order"
     t.index ["section_id"], name: "index_food_items_on_section_id"
   end
 
