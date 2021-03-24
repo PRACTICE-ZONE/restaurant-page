4bf6534afa1b656110d6111364bb4bbc43ea6e29
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-24 20:16:54 +0000

    adding food item controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -6,18 +6,19 @@ class MenuController < ApplicationController
        @sections = Section.all
     section = Section.find_by(name: params[:section])
     @food_items = section.food_items
-    get_cart
+    # get_cart
+    @cart = Cart.new 
   end
-  def set_cart
-    @cart = Cart.create
-    session[:cart_id] = @cart.id 
-    # @cart 
-  end
-  def get_cart 
-    if session[:cart_id]
-      @cart = Cart.find session[:cart_id]
-    else 
-      set_cart
-    end
-  end
+  # def set_cart
+  #   @cart = Cart.create
+  #   session[:cart_id] = @cart.id 
+  #   # @cart 
+  # end
+  # def get_cart 
+  #   if session[:cart_id]
+  #     @cart = Cart.find session[:cart_id]
+  #   else 
+  #     set_cart
+  #   end
+  # end
 end
